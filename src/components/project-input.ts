0462ce57f2ad4cdf98e3ed05e1b340de30e182ec
Manuel Abruzzo <manuel.abruzzo@patagonian.it>
2022-01-27 17:25:05 -0300

    Webpack added

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,8 +1,8 @@
 // ProjectInput Class
-import { autobind } from '../decorators/autobind.js';
-import { projectState } from '../state/project-state.js';
-import { Validatable, validate } from '../util/validation.js';
-import { Component } from './base-component.js';
+import { autobind } from '../decorators/autobind';
+import { projectState } from '../state/project-state';
+import { Validatable, validate } from '../util/validation';
+import { Component } from './base-component';
 
 export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
   titleInputElement: HTMLInputElement;
